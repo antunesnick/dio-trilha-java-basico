193b9f6ea928dc81a31e2ab91bfed5bb81a1b5c0
antunesnick <nickolasalmeida2@gmail.com>
2024-04-29 17:14:13 -0300

    Create ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+        int accountnumber;
+        String agency;
+        String clientName;
+        double balance;
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite a Agência: ");
+        agency = scanner.nextLine();
+
+        System.out.println("Digite o nome do titular: ");
+        clientName = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta: ");
+        balance = scanner.nextDouble();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        accountnumber = scanner.nextInt();
+
+        scanner.close();
+        
+        System.out.println("Olá " + clientName + " obrigado por criar uma conta em nosso banco, sua agência é " + agency + " conta " + accountnumber + " e seu saldo "+ balance + " já está disponível para saque");
+
+    }   
 }
